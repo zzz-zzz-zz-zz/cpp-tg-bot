742c356d2c60c35ea4e3c7e3cf8b928069670530
julia <kharkovsky@protonmail.com>
2018-03-03 23:31:31 +0200

    Some fixes

--- tg-bot/objects/photosize.cpp
+++ tg-bot/objects/photosize.cpp
@@ -7,8 +7,6 @@ PhotoSize::PhotoSize(string jstr) : PhotoSize(json::parse(jstr))
 
 PhotoSize::PhotoSize(json j)
 {
-    std::cout << "PhotoSize: " << j.dump(2) << std::endl;
-
     file_id = j.at("file_id").get<string>();
     width = j.at("width").get<i32_t>();
     height = j.at("height").get<i32_t>();
--- tg-bot/objects/photosize.hpp
+++ tg-bot/objects/photosize.hpp
@@ -7,13 +7,14 @@
 class PhotoSize
 {
 public:
+    PhotoSize() {}
     PhotoSize(string jstr);
     PhotoSize(json j);
 
-    string get_file_id() const { return file_id; }
-    i32_t get_width() const { width > 0 ? width : 0; }
-    i32_t get_height() const { height > 0 ? height : 0; }
-    i32_t get_file_size() const { file_size > 0 ? file_size : 0; }
+    string get_file_id() const noexcept { return file_id; }
+    i32_t get_width() const noexcept { width > 0 ? width : 0; }
+    i32_t get_height() const noexcept { height > 0 ? height : 0; }
+    i32_t get_file_size() const noexcept { file_size > 0 ? file_size : 0; }
 private:
     string file_id;
     i32_t width = -1;
