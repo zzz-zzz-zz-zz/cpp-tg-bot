5612024f11d883a14dfb423871500407a11977f2
julia <kharkovsky@protonmail.com>
2018-03-03 19:20:24 +0200

    std::any support

--- tg-bot/objects/update.cpp
+++ tg-bot/objects/update.cpp
@@ -16,170 +16,244 @@ Update::Update(json j)
         if (j.at("ok").get<bool>())
             j = j["result"];
         else
-            throw std::invalid_argument("\"result\" is not ok!");
+            throw TelegramBadRequestException("Update::Update(json): \"result\" is not ok!");
     )
 
     update_id = j.at("update_id").get<int>();
 
     OMIT(
-        message = new Message(j.at("message").get<json>());
+#ifdef _TGBOT_OPT_UPDATE_ANY
+        upd = std::make_any<Message>(j.at("message").get<json>());
+        upd_type = UpdateType::MESSAGE;
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+        message = std::make_unique<Message>(j.at("message").get<json>());
+#endif
         return;
     )
 
     OMIT(
-        edited_message = new Message(j.at("edited_message").get<json>());
+#ifdef _TGBOT_OPT_UPDATE_ANY
+        upd = std::make_any<Message>(j.at("edited_message").get<json>());
+        upd_type = UpdateType::EDITED_MESSAGE;
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+        edited_message = std::make_unique<Message>(j.at("edited_message").get<json>());
+#endif
         return;
     )
 
     OMIT(
-        channel_post = new Message(j.at("channel_post").get<json>());
+#ifdef _TGBOT_OPT_UPDATE_ANY
+        upd = std::make_any<Message>(j.at("channel_post").get<json>());
+        upd_type = UpdateType::CHANNEL_POST;
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+        channel_post = std::make_unique<Message>(j.at("channel_post").get<json>());
+#endif
         return;
     )
 
     OMIT(
-        edited_channel_post = new Message(j.at("edited_channel_post").get<json>());
+#ifdef _TGBOT_OPT_UPDATE_ANY
+        upd = std::make_any<Message>(j.at("edited_channel_post").get<json>());
+        upd_type = UpdateType::EDITED_CHANNEL_POST;
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+        edited_channel_post = std::make_unique<Message>(j.at("edited_channel_post").get<json>());
+#endif
         return;
     )
 
     OMIT(
-        inline_query = new InlineQuery(j.at("inline_query").get<json>());
+#ifdef _TGBOT_OPT_UPDATE_ANY
+        upd = std::make_any<InlineQuery>(j.at("inline_query").get<json>());
+        upd_type = UpdateType::INLINE_QUERY;
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+        inline_query = std::make_unique<InlineQuery>(j.at("inline_query").get<json>());
+#endif
         return;
     )
 
     OMIT(
-        chosen_inline_result = new ChosenInlineResult(j.at("chosen_inline_result").get<json>());
+#ifdef _TGBOT_OPT_UPDATE_ANY
+        upd = std::make_any<ChosenInlineResult>(j.at("chosen_inline_result").get<json>());
+        upd_type = UpdateType::CHOSEN_INLINE_RESULT;
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+        chosen_inline_result = std::make_unique<ChosenInlineResult>(j.at("chosen_inline_result").get<json>());
+#endif
         return;
     )
 
     OMIT(
-        callback_query = new CallbackQuery(j.at("callback_query").get<json>());
+#ifdef _TGBOT_OPT_UPDATE_ANY
+        upd = std::make_any<CallbackQuery>(j.at("callback_query").get<json>());
+        upd_type = UpdateType::CALLBACK_QUERY;
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+        callback_query = std::make_unique<CallbackQuery>(j.at("callback_query").get<json>());
+#endif
         return;
     )
 
     OMIT(
-        shipping_query = new ShippingQuery(j.at("shipping_query").get<json>());
+#ifdef _TGBOT_OPT_UPDATE_ANY
+        upd = std::make_any<ShippingQuery>(j.at("shipping_query").get<json>());
+        upd_type = UpdateType::SHIPPING_QUERY;
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+        shipping_query = std::make_unique<ShippingQuery>(j.at("shipping_query").get<json>());
+#endif
         return;
     )
 
     OMIT(
-        pre_checkout_query = new PreCheckoutQuery(j.at("pre_checkout_query").get<json>());
+#ifdef _TGBOT_OPT_UPDATE_ANY
+        upd = std::make_any<PreCheckoutQuery>(j.at("pre_checkout_query").get<json>());
+        upd_type = UpdateType::PRE_CHECKOUT_QUERY;
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+        pre_checkout_query = std::make_unique<PreCheckoutQuery>(j.at("pre_checkout_query").get<json>());
+#endif
         return;
     )
 }
 
 Message Update::get_message() 
 {
-    if (message == nullptr)
-        throw std::invalid_argument("Update::get_message() - message is empty!");
-
+#if _TGBOT_OPT_UPDATE_ANY
+    try
+    {
+        return std::any_cast<Message>(upd);
+    }
+    catch (const std::bad_any_cast &e)
+    {
+        std::cout << "Update::get_message(): " << e.what() << std::endl;
+    }
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+    if (!message)
+        throw TelegramNullObjectException("Update::get_message() - message is empty!");
     return *message;
+#endif
 }
 
 Message Update::get_edited_message() 
 {
-    if (edited_message == nullptr)
-        throw std::invalid_argument("Update::get_edited_message() - edited_message is empty!");
-
+#if _TGBOT_OPT_UPDATE_ANY
+    return get_message();
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+    if (!edited_message)
+        throw TelegramNullObjectException("Update::get_edited_message() - edited_message is empty!");
     return *edited_message;
+#endif
 }
 
 Message Update::get_channel_post() 
 {
-    if (channel_post == nullptr)
-        throw std::invalid_argument("Update::get_channel_post() - channel_post is empty!");
-
+#ifdef _TGBOT_OPT_UPDATE_ANY
+    return get_message();
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+    if (!channel_post)
+        throw TelegramNullObjectException("Update::get_channel_post() - channel_post is empty!");
     return *channel_post;
+#endif
 }
 
 Message Update::get_edited_channel_post() 
 {
-    if (edited_channel_post == nullptr)
-        throw std::invalid_argument("Update::get_edited_channel_post() - edited_channel_post is empty!");
-
+#if _TGBOT_OPT_UPDATE_ANY
+    return get_message();    
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+    if (!edited_channel_post)
+        throw TelegramNullObjectException("Update::get_edited_channel_post() - edited_channel_post is empty!");
     return *edited_channel_post;
+#endif
 }
 
 InlineQuery Update::get_inline_query() 
 {
-    if (inline_query == nullptr)
-        throw std::invalid_argument("Update::get_inline_query() - inline_query is empty!");
-
+#ifdef _TGBOT_OPT_UPDATE_ANY
+    try
+    {
+        return std::any_cast<InlineQuery>(upd);
+    }
+    catch (const std::bad_any_cast &e)
+    {
+        std::cout << "Update::get_inline_query(): " << e.what() << std::endl;
+    }
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+    if (!inline_query)
+        throw TelegramNullObjectException("Update::get_inline_query() - inline_query is empty!");
     return *inline_query;
+#endif
 }
 
 ChosenInlineResult Update::get_chosen_inline_result() 
 {
-    if (chosen_inline_result == nullptr)
-        throw std::invalid_argument("Update::get_chosen_inline_result() - chosen_inline_result is empty!");
-
+#ifdef _TGBOT_OPT_UPDATE_ANY
+    try
+    {
+        return std::any_cast<ChosenInlineResult>(upd);
+    }
+    catch (const std::bad_any_cast &e)
+    {
+        std::cout << "Update::get_chosen_inline_result(): " << e.what() << std::endl;
+    }
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+    if (!chosen_inline_result)
+        throw TelegramNullObjectException("Update::get_chosen_inline_result() - chosen_inline_result is empty!");
     return *chosen_inline_result;
+#endif
 }
 
 CallbackQuery Update::get_callback_query() 
 {
-    if (callback_query == nullptr)
-        throw std::invalid_argument("Update::get_callback_query() - callback_query is empty!");
-
+#ifdef _TGBOT_OPT_UPDATE_ANY
+    try
+    {
+        return std::any_cast<CallbackQuery>(upd);
+    }
+    catch (const std::bad_any_cast &e)
+    {
+        std::cout << "Update::get_callback_query(): " << e.what() << std::endl;
+    }
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+    if (!callback_query)
+        throw TelegramNullObjectException("Update::get_callback_query() - callback_query is empty!");
     return *callback_query;
+#endif
 }
 
 ShippingQuery Update::get_shipping_query()
 {
-    if (shipping_query == nullptr)
-        throw std::invalid_argument("Update::get_shipping_query() - shipping_query is empty!");
-
+#ifdef _TGBOT_OPT_UPDATE_ANY
+    try
+    {
+        return std::any_cast<ShippingQuery>(upd);
+    }
+    catch (const std::bad_any_cast &e)
+    {
+        std::cout << "Update::get_shipping_query(): " << e.what() << std::endl;
+    }
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+    if (!shipping_query)
+        throw TelegramNullObjectException("Update::get_shipping_query() - shipping_query is empty!");
     return *shipping_query;
+#endif
 }
 
 PreCheckoutQuery Update::get_pre_checkout_query()
 {
-    if (pre_checkout_query == nullptr)
-        throw std::invalid_argument("Update::get_pre_checkout_query() - pre_checkout_query is empty!");
-        
+#ifdef _TGBOT_OPT_UPDATE_ANY
+    try
+    {
+        return std::any_cast<PreCheckoutQuery>(upd);
+    }
+    catch (const std::bad_any_cast &e)
+    {
+        std::cout << "Update::get_pre_checkout_query(): " << e.what() << std::endl;
+    }
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+    if (!pre_checkout_query)
+        throw TelegramNullObjectException("Update::get_pre_checkout_query() - pre_checkout_query is empty!");   
     return *pre_checkout_query;
+#endif
 }
 
-// TODO: REMAKE WITH variant
 Update::~Update()
 {
-    // Make OK destructor bcz of MEMORY LEAKS
-    return;
-    // CODE BELOW NEVER EXECUTES!!!!!!!!!!!!
 
-    if (message != nullptr) {
-        delete message;
-        return;
-    }
-    if (edited_message != nullptr) {
-        delete edited_message;
-        return;
-    };
-    if (channel_post != nullptr) {
-        delete channel_post;
-        return;
-    }
-    if (edited_channel_post != nullptr) {
-        delete edited_channel_post;
-        return;
-    }
-    if (inline_query != nullptr) {
-        delete inline_query;
-        return;
-    }
-    if (chosen_inline_result != nullptr) {
-        delete chosen_inline_result;
-        return;
-    }
-    if (callback_query != nullptr) {
-        delete callback_query;
-        return;
-    }
-    if (shipping_query != nullptr) {
-        delete shipping_query;
-        return;
-    }
-    if (pre_checkout_query != nullptr) {
-        delete pre_checkout_query;
-        return;
-    }
 }
--- tg-bot/objects/update.hpp
+++ tg-bot/objects/update.hpp
@@ -3,6 +3,9 @@
 
 #include "_all.hpp"
 
+#define _TGBOT_OPT_UPDATE_ANY 1
+#define _TGBOT_OPT_UPDATE_UNIQ_PTR 0
+#define _TGBOT_OPT_UPDATE_RAW_PTR 0
 
 class Update
 {
@@ -22,6 +25,17 @@ public:
     ShippingQuery get_shipping_query();
     PreCheckoutQuery get_pre_checkout_query();
 
+#if _TGBOT_OPT_UPDATE_ANY
+    bool has_message() { return upd_type == UpdateType::MESSAGE; }
+    bool has_edited_message() { return upd_type == UpdateType::EDITED_MESSAGE; }
+    bool has_channel_post() { return upd_type == UpdateType::CHANNEL_POST; }
+    bool has_edited_channel_post() { return upd_type == UpdateType::EDITED_CHANNEL_POST; }
+    bool has_inline_query() { return upd_type == UpdateType::INLINE_QUERY; }
+    bool has_chosen_inline_result() { return upd_type == UpdateType::CHOSEN_INLINE_RESULT; }
+    bool has_callback_query() { return upd_type == UpdateType::CALLBACK_QUERY; }
+    bool has_shipping_query() { return upd_type == UpdateType::SHIPPING_QUERY; }
+    bool has_pre_checkout_query() { return upd_type == UpdateType::PRE_CHECKOUT_QUERY; }
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
     bool has_message() { return message != nullptr; }
     bool has_edited_message() { return edited_message != nullptr; }
     bool has_channel_post() { return channel_post != nullptr; }
@@ -31,22 +45,32 @@ public:
     bool has_callback_query() { return callback_query != nullptr; }
     bool has_shipping_query() { return shipping_query != nullptr; }
     bool has_pre_checkout_query() { return pre_checkout_query != nullptr; }
+#endif
 
-    // TODO: REMAKE WITH variant
     ~Update();
 private:
     long int update_id;
-    // Notice: ONLY ONE OF THIS WILL BE FILLED.
-    // TODO: make in union/std::variant for efficiency
-    Message *message = nullptr;
-    Message *edited_message = nullptr;
-    Message *channel_post = nullptr;
-    Message *edited_channel_post = nullptr;
-    InlineQuery *inline_query = nullptr;
-    ChosenInlineResult *chosen_inline_result = nullptr;
-    CallbackQuery *callback_query = nullptr;
-    ShippingQuery *shipping_query = nullptr;
-    PreCheckoutQuery *pre_checkout_query = nullptr;
+
+#if _TGBOT_OPT_UPDATE_ANY
+    enum class UpdateType {
+        NONE, MESSAGE, EDITED_MESSAGE, CHANNEL_POST, EDITED_CHANNEL_POST,
+        INLINE_QUERY, CHOSEN_INLINE_RESULT, CALLBACK_QUERY,
+        SHIPPING_QUERY, PRE_CHECKOUT_QUERY
+    } upd_type;
+
+    std::any upd; 
+#elif _TGBOT_OPT_UPDATE_UNIQ_PTR
+    unique_ptr<Message> message = nullptr;
+    unique_ptr<Message> edited_message = nullptr;
+    unique_ptr<Message> channel_post = nullptr;
+    unique_ptr<Message> edited_channel_post = nullptr;
+    unique_ptr<InlineQuery> inline_query = nullptr;
+    unique_ptr<ChosenInlineResult> chosen_inline_result = nullptr;
+    unique_ptr<CallbackQuery> callback_query = nullptr;
+    unique_ptr<ShippingQuery> shipping_query = nullptr;
+    unique_ptr<PreCheckoutQuery> pre_checkout_query = nullptr;
+#endif
+
 };
 
 
