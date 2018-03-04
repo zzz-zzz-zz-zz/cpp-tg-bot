bb16c480f9bea00fa7107a8e808c9b959bbd22a2
julia <kharkovsky@protonmail.com>
2018-03-04 14:56:36 +0200

    Removed UpdateFlags

--- tg-bot/bot.cpp
+++ tg-bot/bot.cpp
@@ -23,12 +23,10 @@ void Bot::start_polling(i32_t timeout_s)
     if (cb_OnStart != nullptr)
         cb_OnStart(this);
 
-    if (!update_flags) 
-        throw std::invalid_argument("0 callbacks specified!");
+    if (cb_All != nullptr) 
+    {
+        std::cout << "Bot started polling... Press [Ctrl+C] to exit.\n";
     
-    std::cout << "Bot started polling... Press [Ctrl+C] to exit.\n";
-
-    if (update_flags == Api::UpdateFilters::ALL && cb_All != nullptr) {
         while (true)
         {
             list<Update> upds = api->getUpdates(timeout_s);
@@ -36,30 +34,21 @@ void Bot::start_polling(i32_t timeout_s)
             for (Update &u : upds)
                 cb_All(this, &u);
         }
-    } else {
-        throw std::invalid_argument("No case for not ALL filters!");
+    } 
+    else 
+    {
+        throw std::invalid_argument("No callback for updates!");
     }
 }
 
 
 bool Bot::has_updates()
 {
-    // return api->getUpdates(false).size() > 0;
-    std::cout << "WARNING: Bot.has_updates() always false!!!\n";
-    return false;
-}
-
-void Bot::on_update(i32_t update_filters, std::function<void(Bot*, Update*)> callback)
-{
-    if (update_filters == Bot::UpdateFilters::ALL) {
-        on_update(callback);
-        return;
-    }
+    return api->getUpdates(false).size() > 0;
 }
 
 void Bot::on_update(std::function<void(Bot*, Update*)> callback)
 {
-    update_flags = Api::UpdateFilters::ALL;
     cb_All = callback;
 }
 
--- tg-bot/bot.hpp
+++ tg-bot/bot.hpp
@@ -14,43 +14,15 @@ public:
     bool has_updates();
 
     void on_start(std::function<void(Bot*)> callback);
-    void on_update(i32_t update_filters, std::function<void(Bot*, Update*)> callback);
     void on_update(std::function<void(Bot*, Update*)> callback);
     void start_polling(i32_t timeout_s);
     void start_polling();
 
     Api *api;
-
-    enum UpdateFilters {
-        NONE                  =   0b000000000,
-        MESSAGE               =   0b000000001,
-        EDITED_MESSAGE        =   0b000000010,
-        CHANNEL_POST          =   0b000000100,
-        EDITED_CHANNEL_POST   =   0b000001000,
-        INLINE_QUERY          =   0b000010000,
-        CHOSEN_INLINE_RESULT  =   0b000100000,
-        CALLBACK_QUERY        =   0b001000000,
-        SHIPPING_QUERY        =   0b010000000,
-        PRE_CHECKOUT_QUERY    =   0b100000000,
-        ALL                   =   0b111111111,
-    };
 private:
-    void (*cb_Message)(Bot*, Update*) = nullptr;
-    void (*cb_Edited_message)(Bot*, Update*) = nullptr;
-    void (*cb_Channel_post)(Bot*, Update*) = nullptr;
-    void (*cb_Edited_channel_post)(Bot*, Update*) = nullptr;
-    void (*cb_Inline_query)(Bot*, Update*) = nullptr;
-    void (*cb_Chosen_inline_result)(Bot*, Update*) = nullptr;
-    void (*cb_Callback_query)(Bot*, Update*) = nullptr;
-    void (*cb_Shipping_query)(Bot*, Update*) = nullptr;
-    void (*cb_Pre_checkout_query)(Bot*, Update*) = nullptr;
-
-    //void (*cb_All)(Bot*, Update*) = nullptr;
     std::function<void(Bot*, Update*)> cb_All = nullptr;
-
     std::function<void(Bot*)> cb_OnStart = nullptr;
 
-    i32_t update_flags = UpdateFilters::NONE;
 };
 
 
