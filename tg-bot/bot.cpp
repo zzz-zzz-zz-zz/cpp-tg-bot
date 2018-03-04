db4e2c7556b9501a48efbd7c684345fd56e8871e
julia <kharkovsky@protonmail.com>
2018-03-04 14:48:10 +0200

    Lambda support with std::function

--- tg-bot/bot.cpp
+++ tg-bot/bot.cpp
@@ -26,7 +26,7 @@ void Bot::start_polling(i32_t timeout_s)
     if (!update_flags) 
         throw std::invalid_argument("0 callbacks specified!");
     
-    std::cout << "Bot started polling...\n";
+    std::cout << "Bot started polling... Press [Ctrl+C] to exit.\n";
 
     if (update_flags == Api::UpdateFilters::ALL && cb_All != nullptr) {
         while (true)
@@ -49,50 +49,21 @@ bool Bot::has_updates()
     return false;
 }
 
-void Bot::on_update(i32_t update_filters, void (*callback)(Bot *b, Update *u))
+void Bot::on_update(i32_t update_filters, std::function<void(Bot*, Update*)> callback)
 {
     if (update_filters == Bot::UpdateFilters::ALL) {
         on_update(callback);
         return;
     }
-
-    update_flags |= update_filters;                                          
-
-    if (update_filters & Bot::UpdateFilters::MESSAGE)
-        cb_Message = callback;
-
-    if (update_filters & Bot::UpdateFilters::EDITED_MESSAGE)
-        cb_Edited_message = callback;
-
-    if (update_filters & Bot::UpdateFilters::CHANNEL_POST)
-        cb_Channel_post = callback;
-
-    if (update_filters & Bot::UpdateFilters::EDITED_CHANNEL_POST)
-        cb_Edited_channel_post = callback;
-
-    if (update_filters & Bot::UpdateFilters::INLINE_QUERY)
-        cb_Inline_query = callback;
-
-    if (update_filters & Bot::UpdateFilters::CHOSEN_INLINE_RESULT)
-        cb_Chosen_inline_result = callback;
-
-    if (update_filters & Bot::UpdateFilters::CALLBACK_QUERY)
-        cb_Callback_query = callback;
-
-    if (update_filters & Bot::UpdateFilters::SHIPPING_QUERY)
-        cb_Shipping_query = callback;
-
-    if (update_filters & Bot::UpdateFilters::PRE_CHECKOUT_QUERY)
-        cb_Pre_checkout_query = callback;
 }
 
-void Bot::on_update(void (*callback)(Bot *b, Update *u))
+void Bot::on_update(std::function<void(Bot*, Update*)> callback)
 {
     update_flags = Api::UpdateFilters::ALL;
     cb_All = callback;
 }
 
-void Bot::on_start(void (*callback)(Bot*))
+void Bot::on_start(std::function<void(Bot*)> callback)
 {
     cb_OnStart = callback;
 }
--- tg-bot/bot.hpp
+++ tg-bot/bot.hpp
@@ -13,9 +13,9 @@ public:
 
     bool has_updates();
 
-    void on_start(void (*callback)(Bot*));
-    void on_update(i32_t update_filters, void (*callback)(Bot*, Update*));
-    void on_update(void (*callback)(Bot*, Update*));
+    void on_start(std::function<void(Bot*)> callback);
+    void on_update(i32_t update_filters, std::function<void(Bot*, Update*)> callback);
+    void on_update(std::function<void(Bot*, Update*)> callback);
     void start_polling(i32_t timeout_s);
     void start_polling();
 
@@ -45,9 +45,10 @@ private:
     void (*cb_Shipping_query)(Bot*, Update*) = nullptr;
     void (*cb_Pre_checkout_query)(Bot*, Update*) = nullptr;
 
-    void (*cb_All)(Bot*, Update*) = nullptr;
+    //void (*cb_All)(Bot*, Update*) = nullptr;
+    std::function<void(Bot*, Update*)> cb_All = nullptr;
 
-    void (*cb_OnStart)(Bot*) = nullptr;
+    std::function<void(Bot*)> cb_OnStart = nullptr;
 
     i32_t update_flags = UpdateFilters::NONE;
 };
